0114ef0aca9f0fb9ae02dc1878aeebdf2a296b53
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-08-23 22:19:19 +0200

    Interface Son

---
+++ src/day44/Interface/Lastik.java
@@ -0,0 +1,9 @@
+package day44.Interface;
+
+public interface Lastik extends DisDonanim{
+    void ebat();
+
+    public abstract void jant();
+    String RENK="siyah-beyaz yanaklı";
+
+}
---
+++ src/day44/Interface/SahinRunner.java
@@ -0,0 +1,23 @@
+package day44.Interface;
+
+public class SahinRunner {
+
+  public static void main(String[] args) {
+    Sahin sahin = new Sahin();
+
+    sahin.ayna();
+    sahin.ebat();
+    sahin.jant();
+    sahin.kapi();
+    sahin.kaporta();
+    sahin.koltuk();
+    sahin.motor();
+    sahin.sunroof();
+    sahin.yakit();
+    System.out.println("Şahinlerde music : " + IcDonanim.music);
+    System.out.println("Şahinlerde koltuklar : " + IcDonanim.kumas);
+    System.out.println("Şahinlerde iç donanım : " + IcDonanim.RENK); //ahsap
+    System.out.println("Şahinlerde tekerler : " + DisDonanim.RENK); //metalik
+    System.out.println("Şahinlerde Lastikler : " + Lastik.RENK); //siyah-beyaz yanaklı
+  }
+}
