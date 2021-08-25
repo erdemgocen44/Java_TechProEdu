9075d0ff535513c837dc7814184782272419be5a
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-08-25 22:27:58 +0200

    Iterator

--- src/day46/Ders/LinkedList01.java
+++ src/day46/Ders/LinkedList01.java
@@ -42,25 +42,24 @@ public class LinkedList01 {
     System.out.println(ll1);
 
     ll1.removeLastOccurrence("");
-    System.out.println(ll1);
+    System.out.println(ll1); //[mehmet, samet, said, yağmur, merve,  :-), :-( , başarılı]
 
-    LinkedList<Integer> ll3 = new LinkedList<>(Arrays.asList(1,2,3,4,5,6));
-    System.out.println(ll3);
+    LinkedList<Integer> ll3 = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+    System.out.println(ll3); //[1, 2, 3, 4, 5, 6]
 
     ll3.remove(2);
-    System.out.println(ll3);
+    System.out.println(ll3); //[1, 2, 4, 5, 6]
 
     //ll3.remove(33);RTE
-   // System.out.println(ll3);
+    // System.out.println(ll3);
 
     ll3.remove(ll3.indexOf(6));
-    System.out.println(ll3);
+    System.out.println(ll3); //[1, 2, 4, 5]
 
     ll1.removeAll(ll2);
-    System.out.println(ll1);
-
-
+    System.out.println(ll1); //[mehmet, samet, said, yağmur, merve, başarılı]
 
     
+    System.out.println(ll1.get(3));
   }
 }
